337af3ea5efa8b043766eac03a97a43ce6363177
Rishik <rishik3555@gmail.com>
2025-03-26 23:41:33 +0530

    prismaclient fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from "jsonwebtoken";
 // import { JWT_SECRET } from '@repo/backend-common/config';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prismaClient = new PrismaClient(); // Note the .js extension  // Adjust if necessary based on the folder structure
 
